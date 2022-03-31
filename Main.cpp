7adc5cc70f5f7491951b0c0a21d9554acb71762e
Cole Frost <cole.frost@uvm.edu>
2022-03-31 16:58:25 -0400

    added logic for hitting spacebar to start game

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include "GameDriver.h"
+#include <iostream>
 
 int main()
 {
@@ -7,13 +8,21 @@ int main()
     while (game->window.isOpen()) {
         sf::Event event;
         while (game->window.pollEvent(event)) {
+            // Escape key pressed - exit game
             if ((event.type == sf::Event::Closed) || ((event.type == sf::Event::KeyPressed) &&
                 (event.key.code == sf::Keyboard::Escape)))
             {
                 game->window.close();
                 break;
             }
+            // Space key pressed - start game
+            if (((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space)))
+            {
+                game->init_Game();
+                game->state.screen = playing;
+            }
         }
+
         game->drawScreen();
     }
 
